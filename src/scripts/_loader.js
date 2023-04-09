2e53fa043fd457c74de415d7550fdeb32c3e08b1
andrewdreamer13 <isaevandrey9@gmail.com>
2023-04-09 11:00:30 +0300

    Update 2023-04-09T08:00:18.495Z

--- src/scripts/_loader.js
+++
@@ -1,8 +0,0 @@
-const mask = document.querySelector('.mask');
-window.addEventListener('load',function(){
-  mask.classList.add('hide');
-  setTimeout(function(){
-    mask.remove();
-  },600)
-});
-console.log(mask)
--- src/scripts/_vars.js
+++
@@ -1,4 +0,0 @@
-export const index = document.querySelector('.index');
-export const cartPage = document.querySelector('.cart-page');
-export const learnMorePage = document.querySelector('.learn-more-page');
-
